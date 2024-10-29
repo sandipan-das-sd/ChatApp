751f844a8f5000a2954eb8a68874c33e14646469
Sandipan Das <153122580+sandipan-das-sd@users.noreply.github.com>
2024-10-30 02:14:09 +0530

    Update index.js

--- server/socket/index.js
+++ server/socket/index.js
@@ -199,19 +199,28 @@ dotenv.config();
 const app = express();
 
 const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: function (origin, callback) {
+//       const allowedOrigins = [process.env.FRONTEND_URL, 'https://chat-app-psi-three-33.vercel.app'];
+//       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: ['GET', 'POST'], // Allow specific methods
+//     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Allow specific headers
+//     credentials: true,
+//   }
+// });
+
 const io = new Server(server, {
   cors: {
-    origin: function (origin, callback) {
-      const allowedOrigins = [process.env.FRONTEND_URL, 'https://chat-app-psi-three-33.vercel.app'];
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true, // Allow all origins
     methods: ['GET', 'POST'], // Allow specific methods
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Allow specific headers
-    credentials: true,
+    credentials: true, // Allow credentials (cookies, etc.)
   }
 });
 
